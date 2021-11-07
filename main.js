5ccfabc6e0ecbef6ecee112c8db50911583d3f59
EscolaEducaRS <65333639+EscolaEducaRS@users.noreply.github.com>
2021-11-06 23:05:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let enunciado = document.getElementById('enunciadoId')
+
+
+
+
+
+function gerandoAtivi(){
+    enunciado.innerHTML = `
+    <br>Fazer o Livro Diário dos lançamentos a seguir.
+    
+    `
+    //${razonetes} ${balancete} ${resultado} ${demostrativo} ${balançoPatrimonial}
+
+
+    
+}
+
+
